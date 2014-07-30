d2b6d2f315394d1d71ef05182679885001ba1d3e
Simran-Budhwani <simran.budhwani@cumminscollege.in>
2014-07-30 14:29:42 +0530

    Update calc.cpp
    Added square method

--- calc.cpp
+++ calc.cpp
@@ -3,5 +3,13 @@ void main()
 {
   
     cout<<"Welcome to Github";
+    cout<<"Square"<<square(    );
     
+}
+
+double square(double in_num)
+{
+  double out_num;
+  out_num=in_num*in_num;
+  return out_num;
 }
