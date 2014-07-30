4d241b3ad3947b39fde8a6f723f7e812c38128f1
Simran-Budhwani <simran.budhwani@cumminscollege.in>
2014-07-30 14:40:51 +0530

    Update calc.cpp
    Added add method

--- calc.cpp
+++ calc.cpp
@@ -3,13 +3,21 @@ void main()
 {
   
     cout<<"Welcome to Github";
-    cout<<"Square"<<square(    );
-    
+    cout<<"Square"<<square(1.0);
+    cout<<"Square"<<add(4.0); 
 }
 
 double square(double in_num)
 {
   double out_num;
   out_num=in_num*in_num;
+  return out_num;
+}
+
+
+double add(double in_num)
+{
+  double out_num;
+  out_num=in_num+in_num;
   return out_num;
 }
